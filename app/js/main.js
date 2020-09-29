d816fcd5029c6b20fb4ac368fd93151651960664
zayka130192 <zayka130192@meta.ua>
2020-09-29 14:20:10 +0300

    added slider featured products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-
+    $(".rating").rateYo({
+        rating: 4.5,
+        starWidth: "16px",        
+        readOnly: true,
+        spacing: "5px",
+      });
+      $(".slider__products-wrapp").slick({
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        dots: false,
+        prevArrow: '<button class="slick-arrow slick-prew"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></button>',
+      });
 });
