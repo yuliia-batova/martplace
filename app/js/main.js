a9d6fa3c29244587a90d9c9eecb7c051f464214c
zayka130192 <zayka130192@meta.ua>
2020-09-30 20:44:11 +0300

    added Newest Release Products

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,9 @@ $(function(){
         rating: 4.5,
         starWidth: "16px",        
         readOnly: true,
-        spacing: "5px",
+        spacing: "2px",
       });
-      $(".slider__products-wrapp").slick({
+      $(".products__inner--slider").slick({
         slidesToShow: 1,
         adaptiveHeight: true,
         arrows: true,
@@ -13,4 +13,5 @@ $(function(){
         prevArrow: '<button class="slick-arrow slick-prew"><span class="lnr lnr-chevron-left"></span></button>',
         nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></button>',
       });
+      var mixer=mixitup(".products__inner--flex");
 });
