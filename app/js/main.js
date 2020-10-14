d702a01f14dc6ef90f1d4ea6f7f0e3b0163243ce
zayka130192 <zayka130192@meta.ua>
2020-10-14 21:27:29 +0300

    made changes to the slider and title release

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,16 @@ $(function(){
         readOnly: true,
         spacing: "2px",
       });
-      $(".products__inner--slider").slick({
+      $(".slider__inner").slick({
         slidesToShow: 1,
         adaptiveHeight: true,
         arrows: true,
         dots: false,
+        appendArrows: $('.slider__arrows'),
         prevArrow: '<button class="slick-arrow slick-prew"><span class="lnr lnr-chevron-left"></span></button>',
         nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></button>',
       });
+      
+      
       var mixer=mixitup(".products__inner--flex");
 });
